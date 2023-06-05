9a3323b10e490c24e608024bfc99e0886d10b7b0
Chris <chrislewis.davis@gmail.com>
2023-06-05 08:57:29 -0400

    input component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,13 +3,18 @@ import React from 'react';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 
+import GuessInput from '../GuessInput';
+
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  return <>
+    <p>Put a game here!!!</p>
+    <GuessInput />
+  </>;
 }
 
 export default Game;
