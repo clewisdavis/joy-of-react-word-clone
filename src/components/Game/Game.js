7536f6b221c800c8386ae5e303909bfd7b0744a0
Chris <chrislewis.davis@gmail.com>
2023-06-28 08:24:39 -0400

    exercise four

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 import GuessInput from '../GuessInput';
 import PreviousGuesses from '../PreviousGuesses';
+import GameOverBanner from '../GameOverBanner/GameOverBanner';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -12,24 +14,42 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  
+  // Game state variable, running / won / lost are the three options
+  const [gameStatus, setGameStatus] = React.useState('running');
+
   const [guesses, setGuesses] = React.useState([]);
 
   function handleSubmitGuess(tentativeGuess) {
-    //TODO
-    console.log('Received guess', tentativeGuess);
-    setGuesses([...guesses, tentativeGuess]);
+    const nextGuesses = [...guesses, tentativeGuess]
+
+    setGuesses(nextGuesses);
+
+    // check game status
+    if (tentativeGuess.toUpperCase() === answer) {
+      setGameStatus('won');
+    } else if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED) {
+      setGameStatus('lost')
+    }
   }
 
   return <>
     <p>My Word Game!</p>
+    {gameStatus}
     <PreviousGuesses
       guesses={guesses}
       answer={answer}
     />
     <GuessInput
       handleSubmitGuess={handleSubmitGuess}
+      gameStatus={gameStatus}
     />
+    {gameStatus !== 'running' && (
+      <GameOverBanner 
+        gameStatus={gameStatus} 
+        numOfGuesses={guesses.length} 
+        answer={answer}  
+      />
+    )}
   </>;
 }
 
--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function GuessInput({ handleSubmitGuess }) {
+function GuessInput({ handleSubmitGuess, gameStatus }) {
 
   const [tentativeGuess, setTentativeGuess] = React.useState('');
 
@@ -24,6 +24,7 @@ function GuessInput({ handleSubmitGuess }) {
         <label htmlFor="guess-input">Enter guess:</label>
         <input 
           required
+          disabled={gameStatus !== 'running'}
           minLength={5}
           maxLength={5}
           pattern="[a-zA-Z]{5}"
